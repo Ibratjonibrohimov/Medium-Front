e6080d5eb1d2906eea3812623cc942088d6dc49c
Ibrat Ibrohimov <Ibrat.Ibrohimov@greenwhite.uz>
2024-12-10 09:02:42 +0500

    fix

---
+++ src/app/features/settings/settings.component.ts
@@ -0,0 +1,90 @@
+import {Component, OnDestroy, OnInit} from "@angular/core";
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from "@angular/forms";
+import {Router} from "@angular/router";
+import {User} from "../../core/models/user.model";
+import {UserService} from "../../core/services/user.service";
+import {ListErrorsComponent} from "../../shared/list-errors.component";
+import {Errors} from "../../core/models/errors.model";
+import {Subject} from "rxjs";
+import {map, takeUntil} from "rxjs/operators";
+import {marked} from "marked";
+import use = marked.use;
+
+interface SettingsForm {
+  image: FormControl<string>;
+  username: FormControl<string>;
+  bio: FormControl<string>;
+  email: FormControl<string>;
+  password: FormControl<string>;
+}
+
+@Component({
+  selector: "app-settings-page",
+  templateUrl: "./settings.component.html",
+  imports: [ListErrorsComponent, ReactiveFormsModule],
+  standalone: true,
+})
+export class SettingsComponent implements OnInit, OnDestroy {
+  user!: User;
+  settingsForm = new FormGroup<SettingsForm>({
+    image: new FormControl("", {nonNullable: true}),
+    username: new FormControl("", {nonNullable: true}),
+    bio: new FormControl("", {nonNullable: true}),
+    email: new FormControl("", {nonNullable: true}),
+    password: new FormControl("", {
+      validators: [Validators.required],
+      nonNullable: true,
+    }),
+  });
+  errors: Errors | null = null;
+  isSubmitting = false;
+  destroy$ = new Subject<void>();
+
+  constructor(
+    private readonly router: Router,
+    private readonly userService: UserService
+  ) {
+  }
+
+  ngOnInit(): void {
+    this.userService.getCurrentUser()
+      .pipe(map(user => user.user))
+      .subscribe((user) => {
+        console.log(user)
+        this.settingsForm.patchValue(user as Partial<User>)
+      });
+    // this.settingsForm.patchValue(
+    //   this.userService.getCurrentUser().pipe(map(user => user.user)) as Partial<User>
+    // );
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  logout(): void {
+    this.userService.logout();
+  }
+
+  submitForm() {
+    this.isSubmitting = true;
+
+    this.userService
+      .update(this.settingsForm.value)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: ({user}) =>
+          void this.router.navigate(["/profile/", user.username]),
+        error: (err) => {
+          this.errors = err;
+          this.isSubmitting = false;
+        },
+      });
+  }
+}
